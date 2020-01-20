b0631d700c15e86302ae00279f576b6dd11d3b34
Philipp Batroff <batroff@systopia.de>
2020-01-20 15:10:45 +0100

    added better output for api command

--- api/v3/ProvegMailing/Subscribe.php
+++ api/v3/ProvegMailing/Subscribe.php
@@ -31,7 +31,7 @@ function civicrm_api3_proveg_mailing_Subscribe($params) {
   try{
     $subscribeHandler = new CRM_ProvegAPI_MailingSubscribe();
     $subscribeHandler->handle_request($params);
-    return civicrm_api3_create_success('Created Subscription for ');
+    return civicrm_api3_create_success("Created Subscription for {$subscribeHandler->get_contact_id()}");
   } catch (Exception $e) {
     throw new API_Exception("Error parsing Request. Message: '{$e->getMessage()}'");
   }
