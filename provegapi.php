a21c9836980172da445c6f04659966c548e4668c
Jo Franz <franz@systopia.de>
2024-07-10 16:45:41 +0200

    civix upgrade 23.02.1

--- provegapi.php
+++ provegapi.php
@@ -38,7 +38,6 @@ function provegapi_civicrm_alterAPIPermissions($entity, $action, &$params, &$per
   $permissions['proveg_selfservice']['contactdata']   = ['access ProVeg API'];
 }
 
-
 /**
  * Implements hook_civicrm_config().
  *
@@ -49,39 +48,12 @@ function provegapi_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function provegapi_civicrm_xmlMenu(&$files) {
-  _provegapi_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function provegapi_civicrm_install() {
   _provegapi_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function provegapi_civicrm_postInstall() {
-  _provegapi_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function provegapi_civicrm_uninstall() {
-  _provegapi_civix_civicrm_uninstall();
 }
 
 /**
@@ -98,72 +70,6 @@ function provegapi_civicrm_enable() {
   //  $customData->syncOptionGroup(__DIR__ . '/resources/option_group_activity_type.json');
 }
 
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function provegapi_civicrm_disable() {
-  _provegapi_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function provegapi_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _provegapi_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function provegapi_civicrm_managed(&$entities) {
-  _provegapi_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function provegapi_civicrm_caseTypes(&$caseTypes) {
-  _provegapi_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function provegapi_civicrm_angularModules(&$angularModules) {
-  _provegapi_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function provegapi_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _provegapi_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -171,9 +77,8 @@ function provegapi_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function provegapi_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
